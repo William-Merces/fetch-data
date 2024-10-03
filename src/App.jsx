f1fe89d9694460c9fad48b36a06e87eccef8f9fe
William Mercês <engenheirowilliammerces@gmail.com>
2024-10-03 10:17:52 -0500

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,86 @@
+import React, { useEffect } from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+async function fetchData(url) {
+  const rs = await fetch(url)
+  const json = await rs.json()
+  return json
+}
+
+
+export default function App() {
+  const [currentPage, setCurrentPage] = useState('Beef')
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    fetchData('https://www.themealdb.com/api/json/v1/1/categories.php')
+      .then((data) => setCategories(data.categories))
+  }, [])
+
+  const changePage = (e) => {
+    setCurrentPage(e.target.value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+    <div className='flex '>
+      <div className='flex gap-8 justify-center flex-col p-8 shrink-0'>
+        {categories &&
+          categories.map(item => (
+            <Button
+              key={item.idCategory}
+              title={item.strCategory}
+              changePage={changePage}
+            />
+          ))
+        }
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <Page title={currentPage} />
+    </div>
+
   )
 }
 
-export default App
+
+function Button({ title, changePage }) {
+  return (
+    <button value={title} className='bg-cyan-900 text-white' onClick={changePage}>
+      {title}
+    </button>
+  )
+}
+
+
+function Page({ title }) {
+  const [meals, setMeals] = useState([])
+
+  useEffect(() => {
+    fetchData(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${title}`)
+      .then((data) => setMeals(data.meals))
+  }, [title])
+
+  return (
+    <div className='grid pt-8 shadow w-full'>
+      <h1 className='text-center text-4xl font-semibold'>{title}</h1>
+
+      <ul className='w-full flex flex-wrap gap-4'>
+        {meals &&
+          meals.map((item) =>
+            <MealItem img={item.strMealThumb}/>
+          )
+        }
+
+      </ul>
+    </div>
+  )
+}
+
+function MealItem({ img }) {
+  return (
+    <li className='w-20 overflow-hidden'>
+      <img src={img} alt="" className='w-full' />
+    </li>
+  )
+}
